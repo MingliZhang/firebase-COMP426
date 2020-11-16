807e9ee43f64e839b3ff2ff64e22d4548182ed48
MingliZhang <zml2416154671@gmail.com>
2020-11-16 14:04:51 -0500

    posts database feels done for now

--- functions/controllers/posts.js
+++ functions/controllers/posts.js
@@ -8,10 +8,18 @@ const db = admin.firestore();
 
 const postApp = express();
 
-postApp.use(cors({origin: true}));
+postApp.use(cors({ origin: true }));
+
+function checkProperties(obj) {
+  for (let key in obj) {
+      if (obj[key] === null || obj[key] === "")
+          return false;
+  }
+  return true;
+}
 
 postApp.get("/", async (req, res) => {
-  const snapshot = await db.collection("posts").get();
+  const snapshot = await db.collection("posts").orderBy('createdAt','desc').get();
 
   let posts = [];
   snapshot.forEach((doc) => {
@@ -24,34 +32,59 @@ postApp.get("/", async (req, res) => {
 });
 
 postApp.get("/:id", async (req, res) => {
-    const snapshot = await db.collection('posts').doc(req.params.id).get();
+  const snapshot = await db.collection("posts").doc(req.params.id).get();
 
-    const postId = snapshot.id;
-    const postData = snapshot.data();
+  const postId = snapshot.id;
+  const postData = snapshot.data();
 
-    res.status(200).send(JSON.stringify({id: postId, ...postData}));
+  res.status(200).send(JSON.stringify({ id: postId, ...postData }));
 });
 
 postApp.post("/", async (req, res) => {
-  const post = req.body;
-
-  await db.collection("posts").add(post);
-
-  res.status(201).send(post);
+  const data = req.body;
+  const post = {
+    body: data.body,
+    uid: data.uid,
+    userName: data.userName,
+    anonymous: data.anonymous,
+    comments: [],
+    likes: [],
+    createdAt: new Date().toISOString(),
+  };
+  if (checkProperties(post)) {
+    await db
+      .collection("posts")
+      .add(post)
+      .then((doc) => res.status(201).send(doc._path.segments[1]))
+      .catch((err) => console.error(err));
+  } else {
+    res.status(400).send();
+  }
 });
 
 postApp.put("/:id", async (req, res) => {
-    const body = req.body;
-
-    await db.collection('posts').doc(req.params.id).update(body);
-
-    res.status(200).send(body);
+  const data = req.body;
+  const post = {
+    body: data.body,
+    uid: data.uid,
+    userName: data.userName,
+    anonymous: data.anonymous,
+    comments: data.comments,
+    likes: data.likes,
+    lastUpdateAt: new Date().toISOString(),
+  };
+  if (checkProperties(post)){
+  await db.collection("posts").doc(req.params.id).update(post).then((doc) => res.status(200).send(doc._path.segments[1]))
+  .catch((err) => console.error(err));
+  }else{
+    res.status(400).send();
+  }
 });
 
 postApp.delete("/:id", async (req, res) => {
-    await db.collection("posts").doc(req.params.id).delete();
+  await db.collection("posts").doc(req.params.id).delete();
 
-    res.status(200).send();
-})
+  res.status(200).send(true);
+});
 
 exports.posts = functions.https.onRequest(postApp);
